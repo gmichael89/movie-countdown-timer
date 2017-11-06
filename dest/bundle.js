1cf128779ca4ad7d7d253910454affa7ea4f78ac
gmichael89 <g.michael0704@gmail.com>
2017-11-06 11:06:07 +0000

    Updated bundles.

--- dest/bundle.js
+++ dest/bundle.js
@@ -6,7 +6,7 @@
  * @param {Object} options User options
  * @returns {Object} Merged values of defaults and options
  */
-var extend = function ( defaults, options ) {
+var extend = function (defaults, options) {
     var extended = {};
     var prop;
     for (prop in defaults) {
@@ -22,22 +22,33 @@ var extend = function ( defaults, options ) {
     return extended;
 };
 
-MovieTimer = function (){
-    this.isAnimating = false;
-    this.circleCollection = [];
-    this.verticalLinesCollection = [];
+MovieTimer = function (options){
 
-    this.settings = {
-        canvasHeight: 500,
-        canvasWidth: 700,
-        countdownStartTime: 5,
-        numberOfJumpingVerticalLines: 10,
-    };
+    this.components = {};
+
+    this.finished = false;
 
     this.refs = {
         canvas: 'MovieTimer'
     };
 
+    this._defaults = {
+        canvasHeight: 500,
+        canvasWidth: 700,
+        countdownStartTime: 5,
+        numberOfJumpingVerticalLines: 6,
+    };
+
+    this._userOptions = options;
+
+    // Component Collections
+    this.circleCollection = [];
+    this.verticalLinesCollection = [];
+
+    this.settings = extend(this._defaults, this._userOptions);
+
+    // Runtime variables
+    this.isAnimating = false;
     this.endAngle = 0;
 
     // fabric.Object.prototype.originX = 'center';
@@ -45,15 +56,27 @@ MovieTimer = function (){
     fabric.Object.prototype.selectable = false;
 
     this._createCanvas();
-    this._setCommonPoints();
+    this._setCommonCanvasPositions();
     this._drawGuidelines();
     this._drawCircles();
     this._drawVerticalLines();
     this._createCountdownText();
     this._createArc();
+
+    this.on()
+};
+
+MovieTimer.prototype.countdown = function () {
+    this._animateArc();
+    this._animateVerticalLines();
+};
+
+MovieTimer.prototype.onFinish = function () {
+    console.log('Finished');
 };
 
 MovieTimer.prototype._createCanvas = function () {
+
     this.canvas = new fabric.Canvas(this.refs.canvas, {
         width: this.settings.canvasWidth,
         height: this.settings.canvasHeight
@@ -62,19 +85,15 @@ MovieTimer.prototype._createCanvas = function () {
     this.canvas.calcOffset();
 };
 
-MovieTimer.prototype.countdown = function () {
-    this.animateArc();
-    // this.animateCircles();
-    this.animateVerticalLines();
-};
-
-MovieTimer.prototype._setCommonPoints = function () {
+MovieTimer.prototype._setCommonCanvasPositions = function () {
     this.centerX = (this.canvas.getWidth() / 2);
     this.centerY = (this.canvas.getHeight() / 2);
 };
 
-MovieTimer.prototype.animateArc = function () {
+MovieTimer.prototype._animateArc = function () {
+
     var self = this;
+
     fabric.util.animate({
         startValue: 0,
         endValue: 2 * Math.PI,
@@ -89,46 +108,22 @@ MovieTimer.prototype.animateArc = function () {
             if (self.settings.countdownStartTime > 0) {
                 self.isAnimating = true;
                 --self.settings.countdownStartTime;
-                self.countdownText.setText((self.settings.countdownStartTime).toString());
-                self.animateArc();
+                self.components.countdownText.setText((self.settings.countdownStartTime).toString());
+                self._animateArc();
             }
             else {
-                self.countdownText.setVisible(false);
+                self.components.countdownText.setVisible(false);
                 self.isAnimating = false;
+                self.finished = true;
             }
         }
     });
 };
 
-MovieTimer.prototype.animateCircles = function () {
+MovieTimer.prototype._animateVerticalLines = function () {
+
     var self = this;
-    this.circleCollection.forEach(function(circle){
-        // debugger;
-        var ranNum = (Math.floor(Math.random() * 11) - 5);
-        var direction = (Math.round(Math.random())) ? 'left' : 'top';
-        // debugger;
-        console.log(direction, self['center' + (direction == 'left' ? 'X' : 'Y')] - ranNum);
 
-        circle.animate(direction, self['center' + (direction == 'left' ? 'X' : 'Y')] - ranNum, {
-            duration: 100,
-            onChange: self.canvas.renderAll.bind(self.canvas),
-            onComplete: function onComplete() {
-                var innerRanNum = (Math.floor(Math.random() * 11) - 5);
-                var innerDirection = (Math.round(Math.random())) ? 'left' : 'top';
-
-                console.log(direction, self['center' + (direction == 'left' ? 'X' : 'Y')] - ranNum);
-
-                circle.animate(innerDirection, self['center' + (innerDirection == 'left' ? 'X' : 'Y')] - innerRanNum, {
-                    duration: 100,
-                    onComplete: onComplete
-                });
-            }
-        });
-    });
-};
-
-MovieTimer.prototype.animateVerticalLines = function () {
-    var self = this;
     this.verticalLinesCollection.forEach(function(line){
         // debugger;
         var ranXPosition = Math.floor(Math.random() * self.canvas.getWidth());
@@ -137,15 +132,8 @@ MovieTimer.prototype.animateVerticalLines = function () {
             'left': ranXPosition
         }, {
             duration: 100,
-            abort: function(){
-                console.log('stop');
-            },
             onChange: self.canvas.renderAll.bind(self.canvas),
             onComplete: function onComplete() {
-
-                if (self.isAnimating){
-                    this.abort;
-                }
 
                 var innerRanXPosition = Math.floor(Math.random() * self.canvas.getWidth());
 
@@ -161,7 +149,8 @@ MovieTimer.prototype.animateVerticalLines = function () {
 };
 
 MovieTimer.prototype._createCountdownText = function () {
-    this.countdownText = new fabric.Text((this.settings.countdownStartTime).toString(), {
+
+    this.components.countdownText = new fabric.Text((this.settings.countdownStartTime).toString(), {
         visible: true,
         fontSize: 100,
         //lineHeight: 19,
@@ -171,26 +160,26 @@ MovieTimer.prototype._createCountdownText = function () {
         top: (this.canvas.getHeight() / 2)
     });
 
-    this.canvas.add(this.countdownText);
+    this.canvas.add(this.components.countdownText);
 };
 
 MovieTimer.prototype._drawGuidelines = function () {
 
-    var verticalLine = new fabric.Line([
+    var verticalCrosshairLine = new fabric.Line([
         this.centerX, 0, this.centerX, this.canvas.getHeight()
     ], {
         stroke: '#000',
         selectable: false
     });
 
-    var horizontalLine = new fabric.Line([
+    var horizontalCrosshairLine = new fabric.Line([
         0, this.centerY, this.canvas.getWidth(), this.centerY
     ], {
         stroke: '#000',
         selectable: false
     });
 
-    this.canvas.add(verticalLine, horizontalLine);
+    this.canvas.add(verticalCrosshairLine, horizontalCrosshairLine);
 };
 
 MovieTimer.prototype._drawVerticalLines = function () {
@@ -202,7 +191,8 @@ MovieTimer.prototype._drawVerticalLines = function () {
         var verticalLine = new fabric.Line([
             xPosition, 0, xPosition, this.canvas.getHeight()
         ], {
-            stroke: '#000'
+            stroke: '#000',
+            opacity: 0.2
         });
 
         this.verticalLinesCollection.push(verticalLine);
@@ -210,7 +200,6 @@ MovieTimer.prototype._drawVerticalLines = function () {
     }
 
 };
-
 
 MovieTimer.prototype._drawCircles = function () {
 
@@ -242,7 +231,7 @@ MovieTimer.prototype._drawCircles = function () {
 MovieTimer.prototype._createArc = function () {
     var self = this;
 
-    this.arc = new fabric.Circle({
+    this.components.arc = new fabric.Circle({
         radius: (this.canvas.getWidth() * 2),
         left: (this.canvas.getWidth() / 2),
         top: (this.canvas.getHeight() / 2),
@@ -260,9 +249,36 @@ MovieTimer.prototype._createArc = function () {
         }
     });
 
-    this.canvas.add(this.arc);
+    this.canvas.add(this.components.arc);
 };
 
 module.exports = MovieTimer;
+
+// MovieTimer.prototype._animateCircles = function () {
+//     var self = this;
+//     this.circleCollection.forEach(function(circle){
+//         // debugger;
+//         var ranNum = (Math.floor(Math.random() * 11) - 5);
+//         var direction = (Math.round(Math.random())) ? 'left' : 'top';
+//         // debugger;
+//         console.log(direction, self['center' + (direction == 'left' ? 'X' : 'Y')] - ranNum);
+//
+//         circle.animate(direction, self['center' + (direction == 'left' ? 'X' : 'Y')] - ranNum, {
+//             duration: 100,
+//             onChange: self.canvas.renderAll.bind(self.canvas),
+//             onComplete: function onComplete() {
+//                 var innerRanNum = (Math.floor(Math.random() * 11) - 5);
+//                 var innerDirection = (Math.round(Math.random())) ? 'left' : 'top';
+//
+//                 console.log(direction, self['center' + (direction == 'left' ? 'X' : 'Y')] - ranNum);
+//
+//                 circle.animate(innerDirection, self['center' + (innerDirection == 'left' ? 'X' : 'Y')] - innerRanNum, {
+//                     duration: 100,
+//                     onComplete: onComplete
+//                 });
+//             }
+//         });
+//     });
+// };
 
 },{}]},{},[1]);
